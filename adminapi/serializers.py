8d363fe5834f007ba9ed6d47862392fefacb517d
AltusBarry <AltusBarry@users.noreply.github.com>
2015-09-15 16:07:19 +0200

    Issue-6: removed redundant serializer class

--- adminapi/serializers.py
+++ adminapi/serializers.py
@@ -9,24 +9,3 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('id', 'username', 'password', 'first_name', 'last_name', 'email',)
         write_only_fields = ('password',)
         read_only_fields = ('is_staff', 'is_superuser', 'is_active', 'date_joined',)
-
-# TODO Currently not used. Remove when closer to devlop merge and still not needed
-#class LoginSerializer(serializers.Serializer):
-#    username = serializers.CharField()
-#    password = serializers.CharField(style={'input_type': 'password'})
-#
-#    def validate(self, attrs):
-#        username = attrs.get('username')
-#        password = attrs.get('password')
-#
-#        if username and password:
-#            user = authenticate(username=username, password=password)
-#            if user not in User.objects.all():
-#                msg = ('Unable to log in with provided credentials.')
-#                raise exceptions.ValidationError(msg)
-#        else:
-#            msg = ('Must include "username" and "password".')
-#            raise exceptions.ValidationError(msg)
-#
-#        attrs['user'] = user
-#        return attrs
